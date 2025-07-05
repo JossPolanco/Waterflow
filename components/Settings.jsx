b94c73ed3f4d76d1798e8778fb34b697d06c9ac1
Edwihn <a6523150063@utch.edu.mx>
2025-07-04 18:21:04 -0600

    Add the route and some styles

---
+++ components/Settings.jsx
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native"
+
+export default function Settings(){
+    return(
+        <View className='flex items-center py-10 px-6'>
+            <Text className='font-bold text-4xl'>Configuración</Text>
+        </View>
+    )
+}
