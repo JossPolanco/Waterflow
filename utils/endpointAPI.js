746506eb8af8f7eca89a73001bca4c52e937fb13
Josue Polanco <josue224yosi@gmail.com>
2025-07-18 10:46:16 -0600

    - Update endpoint base.

--- utils/endpointAPI.js
+++ utils/endpointAPI.js
@@ -1,4 +1,4 @@
 export default function ApiEndpoint(){
-    let endpoint = new String('https://00ts5pkz-3000.usw3.devtunnels.ms/')
+    let endpoint = new String('https://web-v45wiwru9euv.up-de-fra1-k8s-1.apps.run-on-seenode.com/')
     return endpoint.toString()
 }
