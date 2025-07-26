81d1541edada763be011c455e621f882c894fcaa
Josue Polanco <josue224yosi@gmail.com>
2025-07-25 18:42:21 -0600

    - Fix visual bug.

--- components/Login.jsx
+++ components/Login.jsx
@@ -50,7 +50,7 @@ export default function Login() {
     }
 
     return (
-        <View className="flex-1 justify-center gap-10 bg-blue-200" style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}>
+        <View className="flex-1 justify-center gap-10 bg-blue-200" style={{ paddingTop: insets.top}}>
             <View className="w-full px-10 justify-center items-center pt-10">
                 <View className="justify-center items-center gap-5">
                     <View>
